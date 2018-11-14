d5aa0b10f19de75324a7538cfcad04463f62f690
John <MonSpek@protonmail.com>
2018-11-14 15:09:57 -0500

    removed redundant code

--- test robot.c
+++ test robot.c
@@ -1,11 +1,11 @@
 //TODO: 1) add when button release throw ball
-//TODO 2) find what they want...
+//TODO: 2) find what they fully want...
 
 task main()
 {
-	while(1==1)
+	while(true) //* forever
 	{
-		if(vexRT(Ch2)==1)
+		if(vexRT(Ch2) == 1) //* for channel 2
 		{
 			motor(port2)=127;
 			motor(port3)=127;
@@ -13,38 +13,22 @@ task main()
 			motor(port5)=127;
 
 		}
-		else if(vexRT(Ch2)==1)
+		else if(vexRT(Ch2) == 1) //* for channel 3
 		{
 			motor(port2)=127;
 			motor(port3)=127;
 			motor(port4)=127;
 			motor(port5)=127;
 		}
-		else
+		else 
 		{
 			motor(port2)=0;
 			motor(port3)=0;
 			motor(port4)=0;
 			motor(port5)=0;
 		}
-			if(vexRT(Ch2)==1)
+		if(vexRT[Btn6U] == 1)
 		{
-			motor(port2)=127;
-			motor(port3)=127;
-			motor(port4)=127;
-			motor(port5)=127;
-
-		}
-		else if(vexRT(Ch2)==1)
-		{
-			motor(port2)=127;
-			motor(port3)=127;
-			motor(port4)=127;
-			motor(port5)=127;
-		}
-		else if(vexRT[Btn6U] == 1)
-		{
-			//! I quesed for the ports please change
 			motor(port6) = 127; // add correct port for arm
 			motor(port7) = 127; // add correct port for arm
 		}
