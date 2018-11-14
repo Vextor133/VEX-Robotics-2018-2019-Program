b06ed883ad16c22b568d7aed5af527e039b1a59e
John <MonSpek@protonmail.com>
2018-11-14 14:48:33 -0500

    Update test robot.c

--- test robot.c
+++ test robot.c
@@ -42,6 +42,12 @@ task main()
 			motor(port4)=127;
 			motor(port5)=127;
 		}
+		else if(vexRT[Btn6U] == 1)
+		{
+			//! I quesed for the ports please change
+			motor(port6) = 127; // add correct port for arm
+			motor(port7) = 127; // add correct port for arm
+		}
 		else
 		{
 			motor(port2)=0;
